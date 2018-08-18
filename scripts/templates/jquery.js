b6cef29da7c08c6686d637f0c6c718c857da3634
Jannik Schäfer <me@jschaefer.io>
2018-08-18 20:57:40 +0200

    Add jquery standalone template

---
+++ scripts/templates/jquery.js
@@ -0,0 +1,28 @@
+
+/**
+ * jQuery Template
+ * @type {Object}
+ */
+module.exports = function(config){
+	return {
+		name: 'jquery',
+		install: {
+			dependencies: [
+				'jquery'
+			],
+			files: []
+		},
+		scss: {
+			files: [],
+			includePaths: []
+		},
+		js: {
+			files: [
+				'node_modules/jquery/dist/jquery.js'
+			]
+		},
+		assets: {
+			files: []
+		}
+	}
+};
